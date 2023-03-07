05fd632a36fc73fbb11bbc9a241b7d343541926a
Srimathy <srimathy.mec@gmail.com>
2023-03-07 19:35:06 +0530

    stop method added to ElectricEngine

--- ElectricEngine.java
+++ ElectricEngine.java
@@ -2,4 +2,7 @@ class ElectricEngine{
 	public static void start() {
 		System.out.println("This is electric start method");
 	}
+	public void stop() {
+		System.out.println("Electric stop method");
+	}
 }
